a63c19696b8b047fd104f503010f3763633f7673
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-11-20 21:02:00 +0100

    feat(bootstrap): add host bootstrap templates for game servers

---
+++ apps/maintain/updater/tests/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+run_bootstrap() {
+  printf '%s\n' 'bootstrap ready'
+}
+
+# current lane: bootstrap
+run_bootstrap() {
+  printf '%s\n' 'bootstrap ready'
+}
